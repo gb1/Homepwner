f522e98a42807cbb3b7fc36bcb07de4302a95a9f
Gregor Brett <gregor.brett@gmail.com>
2013-04-02 12:35:34 +0100

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Gregor Brett on 02/04/2013.
+//  Copyright (c) 2013 Gregor Brett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Gregor Brett on 02/04/2013.
+//  Copyright (c) 2013 Gregor Brett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
